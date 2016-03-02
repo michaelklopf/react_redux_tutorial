e9ded15d731050b3db615fecdc1311efbc6dc34d
Michael Klopf <klopf.michael@gmail.com>
2016-03-02 15:58:54 +0100

    Add reducer, to call functions with actions.

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ http://teropa.info/blog/2015/09/10/full-stack-redux-tutorial.html
 Nice to know
 ===
 https://code-cartoons.com/a-cartoon-intro-to-redux-3afb775501a6  
+http://reactkungfu.com/2015/08/pros-and-cons-of-using-immutability-with-react-js/
---
+++ test/reducer_spec.js
@@ -0,0 +1,51 @@
+/*jshint esversion: 6 */
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+  it('handles SET_ENTRIES', () => {
+    const initialState = Map();
+    const action = {type: 'SET_ENTRIES', entries: ['Inception']};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      entries: ['Inception']
+    }));
+  });
+
+  it('handles NEXT', () => {
+    const initialState = fromJS({
+      entries: ['Inception', 'Interstellar']
+    });
+    const action = {type: 'NEXT'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Inception', 'Interstellar']
+      },
+      entries: []
+    }));
+  });
+
+  it('handles VOTE', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Inception', 'Interstellar']
+      },
+      entries: []
+    });
+    const action = {type: 'VOTE', entry: 'Inception'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Inception', 'Interstellar'],
+        tally: {'Inception': 1}
+      },
+      entries: []
+    }));
+  });
+});
