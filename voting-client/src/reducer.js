e0b4f8e14e46149dae11c9461d3d98fb1fe4ad54
Michael Klopf <klopf.michael@gmail.com>
2016-03-07 16:12:48 +0100

    Adding Redux store for front-end.

--- voting-client/src/reducer.js
+++ voting-client/src/reducer.js
@@ -5,7 +5,7 @@ function setState(state, newState) {
   return state.merge(newState);
 }
 
-export default function(action, state = Map()) {
+export default function(state = Map(), action) {
   switch (action.type) {
     case 'SET_STATE':
       return setState(state, action.state);
